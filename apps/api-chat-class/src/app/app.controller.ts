2802411e7e5ef5b2a9d14134d854df1be8e15728
Carlos Aquino <carlos.aquino.ext@infracommerce.com.br>
2022-01-14 07:34:53 -0300

    feat(start): starting APIs - microservices

--- apps/api-chat-class/src/app/app.controller.ts
+++ apps/api-chat-class/src/app/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   @Get()
   getData() {
     console.log('=> Variavel ', process.env.VAR_C);
-    console.log('=> APPLICATION_NAME ', process.env.APPLICATION_NAME);
+    console.log('=> :: APPLICATION_NAME :: ', process.env.APPLICATION_NAME);
     return this.appService.getData();
   }
 }
